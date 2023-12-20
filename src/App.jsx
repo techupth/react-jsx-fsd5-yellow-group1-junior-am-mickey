105d2243a952faaedc71b5d62eea4eb0c1333ad0
junior <jr.foto@hotmail.com>
2023-12-21 01:10:46 +0700

    junior-ex

--- src/App.jsx
+++ src/App.jsx
@@ -10,28 +10,42 @@ function getCurrentDateTime() {
   const seconds = String(now.getSeconds()).padStart(2, "0");
   return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 }
+const articleStyles = {
+  container: "article",
+  title: "article-title",
+  body: "article-body",
+  link: "article-link",
+};
 
 function IntroSection() {
   return (
     <div>
-      <h3>TechUp Thailand</h3>
-      <p>เตรียมพบกับหลักสูตรปั้นคุณเป็น Software Developer ภายใน 4 เดือน</p>
-      <a href="https://www.techupth.com/" target="blank">
+      <h3 className="article-title">TechUp Thailand</h3>
+      <p className="article-body">
+        เตรียมพบกับหลักสูตรปั้นคุณเป็น Software Developer ภายใน 4 เดือน
+      </p>
+      <a
+        className="article-link"
+        href="https://www.techupth.com/"
+        target="blank"
+      >
         techupth.com
       </a>
-      {/* ให้แสดงวันเวลาด้วย Function `getCurrentDateTime` ที่กำหนดไว้ให้ */}
+      <div className="bootcamp-start-time">
+        <p>วันที่เริ่มหลักสูตร คือ {getCurrentDateTime()}</p>
+      </div>
     </div>
   );
 }
 
 function LoginForm() {
   return (
-    <div class="login-container">
-      <form class="login-form">
+    <div className="login-container">
+      <form className="login-form">
         <h2>Login</h2>
-        <label for="username">Username</label>
+        <label htmlFor="username">Username</label>
         <input type="text" id="username" name="username" />
-        <label for="password">Password</label>
+        <label htmlFor="password">Password</label>
         <input type="password" id="password" name="password" />
         <button type="submit">Login</button>
       </form>
